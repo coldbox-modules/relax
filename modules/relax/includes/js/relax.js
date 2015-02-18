c5c8318da5526aae5a8e723a8f1f071e237a08dc
Luis Majano <lmajano@gmail.com>
2015-02-18 11:31:21 -0800

    new theme

---
+++ modules/relax/includes/js/relax.js
@@ -0,0 +1,320 @@
+var app = function() {
+
+    var init = function() {
+
+        tooltips();
+        toggleMenuLeft();
+        toggleMenuRight();
+        menu();
+        togglePanel();
+        closePanel();
+    };
+
+    var tooltips = function() {
+        $('#toggle-left').tooltip();
+    };
+
+    var togglePanel = function() {
+        $('.actions > .fa-chevron-down').click(function() {
+            $(this).parent().parent().next().slideToggle('fast');
+            $(this).toggleClass('fa-chevron-down fa-chevron-up');
+        });
+
+    };
+
+    var toggleMenuLeft = function() {
+        $('#toggle-left').bind('click', function(e) {
+            if (!$('.sidebarRight').hasClass('.sidebar-toggle-right')) {
+                $('.sidebarRight').removeClass('sidebar-toggle-right');
+                $('.main-content-wrapper').removeClass('main-content-toggle-right');
+            }
+            $('.sidebar').toggleClass('sidebar-toggle');
+            $('.main-content-wrapper').toggleClass('main-content-toggle-left');
+            e.stopPropagation();
+        });
+    };
+
+    var toggleMenuRight = function() {
+        $('#toggle-right').bind('click', function(e) {
+
+            if (!$('.sidebar').hasClass('.sidebar-toggle')) {
+                $('.sidebar').addClass('sidebar-toggle');
+                $('.main-content-wrapper').addClass('main-content-toggle-left');
+            }
+            
+            $('.sidebarRight').toggleClass('sidebar-toggle-right animated bounceInRight');
+            $('.main-content-wrapper').toggleClass('main-content-toggle-right');
+
+            if ( $(window).width() < 660 ) {
+                $('.sidebar').removeClass('sidebar-toggle');
+                $('.main-content-wrapper').removeClass('main-content-toggle-left main-content-toggle-right');
+             };
+
+            e.stopPropagation();
+        });
+    };
+
+    var closePanel = function() {
+        $('.actions > .fa-times').click(function() {
+            $(this).parent().parent().parent().fadeOut();
+        });
+
+    }
+
+    var menu = function() {
+        $("#leftside-navigation .sub-menu > a").click(function(e) {
+            $("#leftside-navigation ul ul").slideUp();
+            if (!$(this).next().is(":visible")) {
+                $(this).next().slideDown();
+            }
+              e.stopPropagation();
+        });
+    };
+    //End functions
+
+    //Dashboard functions
+    var timer = function() {
+        $('.timer').countTo();
+    };
+
+
+    //Vector Maps 
+    var map = function() {
+        $('#map').vectorMap({
+            map: 'world_mill_en',
+            backgroundColor: 'transparent',
+            regionStyle: {
+                initial: {
+                    fill: '#1ABC9C',
+                },
+                hover: {
+                    "fill-opacity": 0.8
+                }
+            },
+            markerStyle: {
+                initial: {
+                    r: 10
+                },
+                hover: {
+                    r: 12,
+                    stroke: 'rgba(255,255,255,0.8)',
+                    "stroke-width": 3
+                }
+            },
+            markers: [{
+                latLng: [27.9881, 86.9253],
+                name: '36 Employees',
+                style: {
+                    fill: '#E84C3D',
+                    stroke: 'rgba(255,255,255,0.7)',
+                    "stroke-width": 3
+                }
+            }, {
+                latLng: [48.8582, 2.2945],
+                name: '58 Employees',
+                style: {
+                    fill: '#E84C3D',
+                    stroke: 'rgba(255,255,255,0.7)',
+                    "stroke-width": 3
+                }
+            }, {
+                latLng: [-40.6892, -74.0444],
+                name: '109 Employees',
+                style: {
+                    fill: '#E84C3D',
+                    stroke: 'rgba(255,255,255,0.7)',
+                    "stroke-width": 3
+                }
+            }, {
+                latLng: [34.05, -118.25],
+                name: '85 Employees ',
+                style: {
+                    fill: '#E84C3D',
+                    stroke: 'rgba(255,255,255,0.7)',
+                    "stroke-width": 3
+                }
+            }]
+        });
+
+    };
+
+    var weather = function() {
+        var icons = new Skycons({
+            "color": "white"
+        });
+
+        icons.set("clear-day", Skycons.CLEAR_DAY);
+        icons.set("clear-night", Skycons.CLEAR_NIGHT);
+        icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+        icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+        icons.set("cloudy", Skycons.CLOUDY);
+        icons.set("rain", Skycons.RAIN);
+        icons.set("sleet", Skycons.SLEET);
+        icons.set("snow", Skycons.SNOW);
+        icons.set("wind", Skycons.WIND);
+        icons.set("fog", Skycons.FOG);
+
+        icons.play();
+    }
+
+    //morris pie chart
+    var morrisPie = function() {
+
+        Morris.Donut({
+            element: 'donut-example',
+            data: [{
+                    label: "Chrome",
+                    value: 73
+                }, {
+                    label: "Firefox",
+                    value: 71
+                }, {
+                    label: "Safari",
+                    value: 69
+                }, {
+                    label: "Internet Explorer",
+                    value: 40
+                }, {
+                    label: "Opera",
+                    value: 20
+                }, {
+                    label: "Android Browser",
+                    value: 10
+                }
+
+            ],
+            colors: [
+                '#1abc9c',
+                '#293949',
+                '#e84c3d',
+                '#3598db',
+                '#2dcc70',
+                '#f1c40f'
+            ]
+        });
+    }
+
+    //Sliders
+    var sliders = function() {
+        $('.slider-span').slider()
+    };
+
+
+    //return functions
+    return {
+        init: init,
+        timer: timer,
+        map: map,
+        sliders: sliders,
+        weather: weather,
+        morrisPie: morrisPie
+
+    };
+}();
+
+//Load global functions
+$(document).ready(function() {
+    app.init();
+
+});
+
+/**
+ * A-la-Carte closing of remote modal windows
+ * @return
+ */
+function closeRemoteModal(){
+	$remoteModal.data("overlay").close();
+	$remoteModalContent.html('').html($remoteModalLoading);
+}
+/**
+ * Open a new remote modal window Ajax style.
+ * @param url The URL ajax destination
+ * @param data The data packet to send
+ * @param w The width of the modal
+ * @param h The height of the modal
+ * @return
+ */
+function openRemoteModal(url,params,w,h){
+	$remoteModal.overlay({
+			mask: {
+			color: '#fff',
+			loadSpeed: 200,
+			opacity: 0.6
+		},
+		closeOnClick: true,
+		onBeforeLoad : function(){
+			$remoteModalContent.load( $remoteModal.data("url"),$remoteModal.data("params") );
+		},
+		onClose: function(){ closeRemoteModal(); }
+	});
+	$remoteModal.find("a.close").attr("title","Close Window");
+	// Set data for this remote modal
+	$remoteModal.data("url",url).data("params",params);
+	// width/height
+	if( w ){ $remoteModal.css("width",w); }
+	if( h ){ $remoteModal.css("height",h); }
+	// open the remote modal
+	$remoteModal.data("overlay").load();
+}
+/**
+ * Activate modal confirmation windows
+ * @return
+ */
+function activateConfirmations(){
+	
+	// verify overlay already loaded
+	if( !$confirmIt.data("overlay") ){
+		// Overlay the global confirmation
+		$confirmIt.overlay({
+			mask: {
+				color: '#fff',
+				loadSpeed: 200,
+				opacity: 0.6
+			},
+			closeOnClick:false
+		});
+		
+		// close button triggers for confirmation dialog
+		$confirmIt.find("button").click(function(e){
+			if( $(this).attr("data-action") == "confirm" ){
+				window.location =  $confirmIt.data('confirmSrc');
+			}
+		});
+	}
+	
+	// Activate dynamic confirmations from <a> of class confirmIt
+	$('a.confirmIt').click(function(e){
+		// setup the href
+		$confirmIt.data("confirmSrc", $(this).attr('href'));
+		// data-message
+		if( $(this).attr('data-message') ){
+			$confirmIt.find("#confirmItMessage").html( $(this).attr('data-message') );
+		}
+		// data-title
+		if( $(this).attr('data-title') ){
+			$confirmIt.find("#confirmItTitle").html( $(this).attr('data-title') );
+		}
+		// show the confirmation when clicked
+		$confirmIt.data("overlay").load();
+		// prevent default action
+		e.preventDefault();
+	});
+}
+function formatJSON(id){
+	$("#"+id).val( formatJSONRaw( $("#"+id).val() ) ); 
+}
+function formatJSONRaw(jsonData){
+	if( !jsonData.length ){ return ''; }
+	try{
+		var result = jsonlint.parse( jsonData );
+		if (result) {
+			// Reformat and replace double-escaped slashes:
+			return JSON.stringify(result, false, 4).replace(/\\\\/g, "\\");
+		}
+		return jsonData;
+	}
+	catch(e){
+		console.log(e);
+		return jsonData;
+	}
+}
