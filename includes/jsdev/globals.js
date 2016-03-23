2d0e697162d71a95d8158ee1fdd522646694e3e1
Jon Clausen <jon_clausen@silowebworks.com>
2016-03-23 08:19:06 -0400

    add annotation on file implementation

--- includes/jsdev/globals.js
+++ includes/jsdev/globals.js
@@ -1,3 +1,7 @@
+/**
+* This is the globals placeholder file.  
+* It maintains the core RJS config and will be appended with the global `deps` specified in /require.build.js
+**/
 requirejs.config({
 	baseUrl: '/modules/relax/includes/js/',
 	shim: {
